a6c004880cd583f0ea96791627700212ce1ef51b
matthieuthuetdev <mthuet.pro@gmail.com>
2024-11-12 07:49:03 +0100

    création de la fonction pars csv

--- function.php
+++ function.php
@@ -1,5 +1,23 @@
 <?php
-function csvToHtml($fileName)
-{
-    
+function csvToHtml($fileName) {
+    $file = file_get_contents($fileName);
+    $class = explode(".",$fileName)[0];
+    $htmlTable = "<table class='$class'>";
+    $htmlTable .= "<thead> <tr>";
+    $thead = explode(",",$file[0]);
+    foreach($thead as $collTitle){
+        $htmlTable .= "<th>$callTitle</th>";
+    }
+    $htmlTable .= "</tr></thead><tbody>";
+for ($i=1; $i < count($file); $i++) { 
+    $line = explode(",",$file[$î])
+}
+    $htmlTable .= "</tbody></table>";
+
+
+
+
+
+
+    $htmlTable .= "</table>"
 }
